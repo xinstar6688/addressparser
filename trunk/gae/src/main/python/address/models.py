b3d10a53115f692b24a49e03c7cc9c0658461d7e
kuisong.tong <kuisong.tong@f8fc5d3a-5755-0410-ad62-238e6619df79>
2008-09-02 14:37:58 +0000

    finish area parser

--- trunk/gae/src/main/python/address/models.py
+++ trunk/gae/src/main/python/address/models.py
@@ -1,4 +1,88 @@
 from google.appengine.api import memcache
+
+class AreaParser:
+    @classmethod
+    def isChild(cls, parent, child):        
+        if child.get("parent", None) == parent["code"] :
+            return True
+        
+        father = AreaCache.getParent(child)
+        if father:
+            return father.get("parent", None) == parent["code"]
+        return False
+    
+    @classmethod
+    def getParents(cls, area):
+        parents = []
+        parent = AreaCache.getParent(area);
+        if parent:
+            parents.append(parent)
+            parents.extend(cls.getParents(parent))    
+        return parents    
+    
+    @classmethod
+    def parse(cls, str, start = 0, parent = None):
+        address = str[start:]
+        for i in range(len(address)):
+            cities = [city for city in AreaCache.getMatchedCities(address[i:]) if  (not parent) or cls.isChild(parent, city)]           
+            
+            if len(cities) > 0:
+                followStart = start + i + len(cities[0]["name"])                
+               
+                if ExcludeWordCache.isStartWith(address[followStart:]):
+                    continue
+                
+                norrowCities = []
+                for city in cities:
+                    if city["hasChild"]:
+                        followCities = cls.parse(str, followStart, city)
+                        if len(followCities) > 0: 
+                            norrowCities.extend(followCities)
+                if len(norrowCities) > 0:
+                    cities = norrowCities
+                                
+                parentCities = []
+                for city in cities:
+                    parentCities.extend(cls.getParents(city)) 
+                 
+                for city in parentCities:
+                    if city in cities:
+                        cities.remove(city)   
+                               
+                if len(cities) > 1:
+                    matchedProvicens = []
+                    for city in cities:
+                        if cls.hasMatchedProvicen(city, str[0:start + i]) or cls.hasMatchedProvicen(city, str[followStart:]):
+                            matchedProvicens.append(city);
+
+                    if len(matchedProvicens) > 0:
+                        return matchedProvicens
+
+                return cities
+            
+        return []
+    
+    @classmethod
+    def hasMatchedProvicen(cls, city, string):
+        parent = AreaCache.getParent(city)
+        if parent:            
+            if cls.hasMatchedParent(parent["name"], string):
+                return True                                     
+            else:
+                return cls.hasMatchedProvicen(parent, string)
+        return False
+    
+    @classmethod
+    def hasMatchedParent(cls, parentName, string):
+        position = string.find(parentName)
+        if position >= 0:
+            followString = string[position + len(parentName):]
+            if ExcludeWordCache.isStartWith(followString):
+                return cls.hasMatchedParent(parentName, followString)
+            else:
+                return True;
+           
+    
 class AbstractCache:   
     @classmethod
     def getCache(cls):
