70871174b55b3ad0c4a463185e316030ca87afff
kuisong.tong <kuisong.tong@f8fc5d3a-5755-0410-ad62-238e6619df79>
2008-09-04 05:35:02 +0000

    add logging

--- gae/src/main/python/address/services.py
+++ gae/src/main/python/address/services.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from address.models import AreaParser, ExcludeWordCache, AreaCache, Area
 from django.utils import simplejson
 from google.appengine.ext.webapp import RequestHandler
@@ -15,13 +17,13 @@ def toJson(area):
 
 class AreaResource(RequestHandler):
     def get(self, code):
-        logging.info("getting area for %s" % code)
+        logging.info("getting area[%s]" % code)
         area = Area.getByCode(code)
         if area:
-            logging.debug("got area for %s" % code)
+            logging.info("got area[%s]" % code)
             self.response.out.write(toJson(area));
         else:
-            logging.debug("area [%s] not found" % code)
+            logging.info("area[%s] not found" % code)
             self.response.set_status(404)
 
 class AreaParserService(RequestHandler):
@@ -30,6 +32,8 @@ class AreaParserService(RequestHandler):
         logging.info("parsing %s" % address)
         
         areas = AreaParser.parse(address)
+        logging.info("got areas[%s] for %s" % (",".join([area.name for area in areas]), address))
+
         body = '{"areas":[%s]}' % ",".join([toJson(area) for area in areas])
         self.response.out.write(body);
         
@@ -41,6 +45,9 @@ class AreasService(RequestHandler):
             self.response.set_status(400)
             return
         
+        code = putArea["code"]
+        logging.info("posting area[%s]" % code)
+        
         area = Area.getByCode(putArea["code"])
         (area, created) = area and (area, False) or (Area(), True)
             
@@ -51,13 +58,18 @@ class AreasService(RequestHandler):
         area.save()
         
         if created:
+            logging.info("area[%s] is created " % code)
             self.response.set_status(201)           
-            self.response.headers["Location"] = "/areas/%s" % area.code
+            self.response.headers["Location"] = "/areas/%s" % code
         else:
+            logging.info("area[%s] is modified" % code)
             self.response.set_status(204)
         
     def delete(self):
+        logging.info("clearing all areas")
         AreaCache.clear()
+        
+        logging.info("all areas are cleared")
         self.response.set_status(204)
         
 class ExcludeWordsService(RequestHandler):
@@ -68,9 +80,13 @@ class ExcludeWordsService(RequestHandler):
             self.response.set_status(400)
             return
         
+        logging.info("putting exclude words[%s]" % ",".join(words))
         ExcludeWordCache.clear()
+        logging.info("exclude words are cleared")
+        
         for word in words:
             ExcludeWordCache.put(word);
+        logging.info("exclude words[%s] are put" % ",".join(words))
             
         self.response.set_status(204)
     
@@ -81,6 +97,9 @@ class ExcludeWordsService(RequestHandler):
             self.response.set_status(400)
             return
         
+        logging.info("posting exclude word[%s]" % word)
         ExcludeWordCache.put(word);
+        logging.info("exclude word[%s] is post" % word)
+
         self.response.set_status(204)
         
